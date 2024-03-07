52ae431494b39d077b94210402c5e46e47987dc3
Pedro Henrique Araújo Souza <161653851+PedrooHAraujoo@users.noreply.github.com>
2024-03-07 17:26:57 -0300

    Teste do Código com list comprehension

--- teste_do_codigo.py
+++ teste_do_codigo.py
@@ -1,5 +1,4 @@
 numeros = list(range(1,101))
-for numero in numeros:
-    if numero % 2==0 and numero % 4==0:
-        print(numero)
-        
+pares_div4 = [numero for numero in numeros if numero % 2==0 and numero % 4==0]
+
+print(pares_div4)
