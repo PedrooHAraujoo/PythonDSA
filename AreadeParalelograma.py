f0861efbc61609f78fd90acbfaa70f10e1b3253a
Pedro Henrique Araújo Souza <161653851+PedrooHAraujoo@users.noreply.github.com>
2024-03-13 15:55:57 -0300

    Correção do codigo do paralelograma

--- AreadeParalelograma.py
+++ AreadeParalelograma.py
@@ -1,4 +1,4 @@
 comp = float(input('Qual o valor do comprimento?'))
 alt = float(input('Qual o valor da altura?'))
-area = (comp + alt)/2
+area = comp * alt
 print('A área do paralelograma é', area)
