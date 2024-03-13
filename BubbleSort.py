4ee8436c0e22e40cd7b2a931fef9084495d8316e
Pedro Henrique Araújo Souza <161653851+PedrooHAraujoo@users.noreply.github.com>
2024-03-13 17:36:08 -0300

    Código de algoritmo Bubble Sort

---
+++ BubbleSort.py
@@ -0,0 +1,11 @@
+lista = [60, 8, 5, 10, 11, 65, 75]
+
+
+def bubble_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+print(bubble_sort(lista))
