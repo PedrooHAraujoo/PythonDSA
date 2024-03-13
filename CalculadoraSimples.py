073428390f47d8fec5fd6237029e95470eda5ae4
Pedro Henrique Araújo Souza <161653851+PedrooHAraujoo@users.noreply.github.com>
2024-03-13 17:02:22 -0300

    Calculadora atualizada com tratamento de erro de divisao por zero

--- CalculadoraSimples.py
+++ CalculadoraSimples.py
@@ -1,15 +1,21 @@
 metodo = input('Qual o método que deseja utilizar? (soma, sub, mult, div)')
-if metodo == 'soma':
-    resultado = float(input('Qual o seu primeiro número? ')) + float(input('Qual o seu segundo número?'))
-    print('A soma dos numeros escolhidos são:', resultado)
-elif metodo == 'sub':
-    resultado = float(input('Qual o seu primeiro número? ')) - float(input('Qual o seu segundo número?'))
-    print('A subtracao dos numeros escolhidos são:', resultado)
-if metodo == 'mult':
-    resultado = float(input('Qual o seu primeiro número? ')) * float(input('Qual o seu segundo número?'))
-    print('A multiplicacao dos numeros escolhidos são:', resultado)
-elif metodo == 'div':
-    resultado = float(input('Qual o seu primeiro número? ')) / float(input('Qual o seu segundo número?'))
-    print('A divisao dos numeros escolhidos são:', resultado)
+if metodo in ['soma', 'sub', 'mult', 'div']:
+    num1 = float(input('Qual o seu primeiro número? '))
+    num2 = float(input('Qual o seu segundo número?'))
+    if metodo == 'soma':
+        resultado = num1 + num2
+        print('A soma dos numeros escolhidos são:', resultado)
+    elif metodo == 'sub':
+        resultado = num1 - num2
+        print('A subtracao dos numeros escolhidos são:', resultado)
+    elif metodo == 'mult':
+        resultado =num1 * num2
+        print('A multiplicacao dos numeros escolhidos são:', resultado)
+    elif metodo == 'div':
+        if num2 != 0:
+            resultado = num1 / num2
+            print('A divisao dos numeros escolhidos são:', resultado)
+        else:
+            print('Essa divisao nao pode ser feita, pois nenhum numero pode ser divisivel por zero!')    
 else:
     print('Esse metodo nao é valido!')
