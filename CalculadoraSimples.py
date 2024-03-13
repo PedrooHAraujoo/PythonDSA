8f7b57ae68f30646ce46b9ae2a32f95f00169c68
Pedro Henrique Araújo Souza <161653851+PedrooHAraujoo@users.noreply.github.com>
2024-03-13 16:20:54 -0300

    Calculadora mais rápida e com o tratamento de erro caso não seja escolhido o método correto

--- CalculadoraSimples.py
+++ CalculadoraSimples.py
@@ -1,14 +1,15 @@
 metodo = input('Qual o método que deseja utilizar? (soma, sub, mult, div)')
 if metodo == 'soma':
-    soma = float(input('Qual o seu primeiro número? ')) + float(input('Qual o seu segundo número?'))
-    print('A soma dos numeros escolhidos são:', soma)
+    resultado = float(input('Qual o seu primeiro número? ')) + float(input('Qual o seu segundo número?'))
+    print('A soma dos numeros escolhidos são:', resultado)
 elif metodo == 'sub':
-    sub = float(input('Qual o seu primeiro número? ')) - float(input('Qual o seu segundo número?'))
-    print('A subtracao dos numeros escolhidos são:', sub)
+    resultado = float(input('Qual o seu primeiro número? ')) - float(input('Qual o seu segundo número?'))
+    print('A subtracao dos numeros escolhidos são:', resultado)
 if metodo == 'mult':
-    mult = float(input('Qual o seu primeiro número? ')) * float(input('Qual o seu segundo número?'))
-    print('A multiplicacao dos numeros escolhidos são:', mult)
+    resultado = float(input('Qual o seu primeiro número? ')) * float(input('Qual o seu segundo número?'))
+    print('A multiplicacao dos numeros escolhidos são:', resultado)
 elif metodo == 'div':
-    div = float(input('Qual o seu primeiro número? ')) / float(input('Qual o seu segundo número?'))
-    print('A divisao dos numeros escolhidos são:', div)
-    
+    resultado = float(input('Qual o seu primeiro número? ')) / float(input('Qual o seu segundo número?'))
+    print('A divisao dos numeros escolhidos são:', resultado)
+else:
+    print('Esse metodo nao é valido!')
